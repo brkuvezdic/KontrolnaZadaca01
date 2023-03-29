32726d33a4d2d509231e6217edcc195841fa2824
brkuvezdic <branimirkuvezdic@gmail.com>
2023-03-29 19:10:29 +0200

    drugipush
    drugi

--- Kontrolna.js
+++ Kontrolna.js
@@ -1,7 +1,52 @@
 //a grupa
 
 //1.
+// function parni() {
+//   for (let i = 0; i <= 100; i++) {
+//     if (i % 2 == 0) console.log(i);
+//   }
+// }
 
-for (let i = 0; i <= 100; i++) {
-  console.log(i);
-}
+// parni();
+
+//2 zad
+//Napisi funkciju koja koristi switch selekciju za pretvaranje numerickog unosa u dan u tjednu. Za neispravan unos ispisi prikladnu poruku.
+//INPUT: 1; OUTPUT: "Ponedjeljak";
+//INPUT: 5; OUTPUT: "Petak";
+
+// let a = 1;
+
+// function dani(a) {
+//   console.log("Unesi dan od 1 do 7");
+//   switch (a) {
+//     case "1":
+//       console.log("Ponedjeljak");
+//       break;
+
+//     case "2":
+//       console.log("Utorak");
+//       break;
+//     case "3":
+//       console.log("srijeda");
+//       break;
+//     case "4":
+//       console.log("cetvrtak");
+//       break;
+//     case "5":
+//       console.log("petak");
+//       break;
+//     case "6":
+//       console.log("subota");
+//       break;
+//     case "7":
+//       console.log("ned");
+//       break;
+
+//     default:
+//       console.log("Krivi unos");
+//   }
+// }
+
+// dani("6");
+
+//3.
